3b281ceca1a779636d140cc90d038dc8c39311d2
Valentin Gomes <valentin_gomes@hotmail.com>
2024-09-30 21:53:06 -0300

    Atividade 16

---
+++ PROJETO_16_VALENTIN/src/main/java/com/mycompany/projeto_16_valentin/PROJETO_16_VALENTIN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_16_valentin;
+
+/**
+ *
+ * @author v.gomes
+ */
+public class PROJETO_16_VALENTIN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
