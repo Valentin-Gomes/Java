e5375c03871c7fe8ba5b011c429d39ee2e77d71b
Valentin Gomes <valentin_gomes@hotmail.com>
2024-10-17 21:47:19 -0300

    Cadastro_Campos

---
+++ Projetos Pratica/PROJETO_CADASTRO_CAMPOS/src/main/java/com/mycompany/projeto_cadastro_campos/PROJETO_CADASTRO_CAMPOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_cadastro_campos;
+
+/**
+ *
+ * @author v.gomes
+ */
+public class PROJETO_CADASTRO_CAMPOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
